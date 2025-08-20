24185b2b13c97907e58922d4ea5c1a1b03da6c42
Eze Henry <henryporsche13@gmail.com>
2025-08-20 22:12:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// For future interactivity if needed
+console.log("Landing page script loaded.");
+// Smooth scroll to products section when "Discover More" button is clicked
+document.getElementById('discover').addEventListener('click', function () {
+  document.getElementById('products').scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
